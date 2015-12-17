df09ed0c52f564faf30bd0e06124ba68c1dc089d
Mitch Amiano <mcamiano@ncsu.edu>
2015-12-17 11:40:07 -0500

    Demonstrate frames-and-slots template and data organization

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Transphporm\Builder;
+
+class HomePage {
+    private $template;
+    private $data=array();
+
+    public function __construct($xmlTemplateFilePath, $tssTransformFilePath) 
+    {
+        $this->template = new Builder($xmlTemplateFilePath, $tssTransformFilePath);
+    }
+
+    public function curry($curriedParameters=array())
+    {
+        if (is_array($curriedParameters)) {
+            $this->data = array_merge($this->data, $curriedParameters);
+        } else {
+            throw new \InvalidArgumentException("HomePage requires an array of simple named scalar values as parameters, something else given");
+        }
+    }
+
+    public function render($overrideData=array())
+    {
+        $this->curry($overrideData);
+        echo var_dump( $f=(object)$this->data);
+        return $this->template->output((object)$this->data)->body;
+    }
+}
+
+$data = array(
+    'pagetitle' => "Awesome Page",
+    'title'     => "Article Title",
+    'subtitle'  => "By Zack",
+    'fruits'    => [ (object)['name'=>'Apple'], (object)['name'=>'Pear'] ]
+); 
+
+$page = 'templates/home.xml';
+$tss = 'templates/home.tss';
+
+$template = new HomePage($page,$tss);
+$template->curry($data);
+echo $template->render();
